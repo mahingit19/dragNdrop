ed96e482c601bcdc6cb4ef3a4aff807858545d9f
Mahinur Rahman <mahin.mail19@gmail.com>
2025-03-06 14:34:54 +0600

    comments added"

--- script.js
+++ script.js
@@ -1,46 +1,63 @@
+// declaring all variables for shorthand
 const uploadButton = document.getElementById('uploadButton');
 const fileInput = document.getElementById('fileInput');
 const messageInput = document.getElementById('messageInput');
 const preview = document.getElementById('preview');
 const fileArray = [];
 
+// for upload button to work as a input
 uploadButton.addEventListener('click', function () {
     fileInput.click();
 });
 
+// for updating each changes
 fileInput.addEventListener('change', handleFiles);
 
+// for clipboard paste
 messageInput.addEventListener('paste', handlePaste);
 
+// adding styles when dragover
 document.addEventListener('dragover', function (event) {
     event.preventDefault();
     document.querySelector('.chat-container').classList.add('drag-over');
 });
 
+// removing the files when drag leaves the area
 document.addEventListener('dragleave', function () {
     document.querySelector('.chat-container').classList.remove('drag-over');
 });
 
+// when files dropped handle function will be called
 document.addEventListener('drop', function (event) {
     event.preventDefault();
     document.querySelector('.chat-container').classList.remove('drag-over');
     handleFiles(event);
 });
 
+// making function for handiling the files
 function handleFiles(event) {
+
+    // if files dropped file should transfer to input
     const files = event.type === 'drop' ? event.dataTransfer.files : event.target.files;
 
+    // for loop until the files end
     for (let i = 0; i < files.length; i++) {
         const file = files[i];
+
+        // using file reader api
         const reader = new FileReader();
 
+        // if reader loads function will execute
         reader.onload = function (e) {
+            // a div will create inside parent
             const previewItem = document.createElement('div');
             previewItem.classList.add('preview-item');
 
+            // an img tag will create and scr will set from files source
             const img = document.createElement('img');
             img.src = e.target.result;
 
+            // for every img a remove button will generate and onclick it will remove from the input
             const removeButton = document.createElement('button');
             removeButton.textContent = 'Remove';
             removeButton.classList.add('remove-button');
@@ -54,14 +71,17 @@ function handleFiles(event) {
             previewItem.appendChild(removeButton);
             preview.appendChild(previewItem);
 
+            // files will be pushed inside the input
             fileArray.push(file);
             updateFileInput();
         };
 
+        // it will read files path
         reader.readAsDataURL(file);
     }
 }
 
+// this fucntion will work when clipboard paste happens
 function handlePaste(event) {
     const items = (event.clipboardData || event.originalEvent.clipboardData).items;
 
@@ -99,6 +119,7 @@ function handlePaste(event) {
     }
 }
 
+// transfering dataTransfer files into input tag
 function updateFileInput() {
     const dataTransfer = new DataTransfer();
     
